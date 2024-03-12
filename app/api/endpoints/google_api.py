18419d0bdab9b9de5e6b6ec65b86a6c2767763a9
Anton <anton_ata@mail.ru>
2024-03-12 15:14:17 +0300

    Fixed google endpoints, services

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -1,12 +1,11 @@
 from aiogoogle import Aiogoogle
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_async_session
 from app.core.google_client import get_service
 from app.core.user import current_superuser
 from app.crud.charity_project import charity_project_crud
-from app.schemas.charity_project import CharityProjectDB
 from app.services.google_api import (set_user_permissions, spreadsheets_create,
                                      spreadsheets_update_value)
 
@@ -15,7 +14,7 @@ router = APIRouter()
 
 @router.post(
     '/',
-    response_model=list[CharityProjectDB],
+    response_model=str,
     dependencies=[Depends(current_superuser)],
 )
 async def get_report(
@@ -26,13 +25,19 @@ async def get_report(
     charity_projects = await (
         charity_project_crud.get_projects_by_completion_rate(session)
     )
-    spreadsheet_url, spreadsheet_id = await spreadsheets_create(
+    spreadsheet_id, spreadsheet_url = await spreadsheets_create(
         wrapper_services
     )
     await set_user_permissions(spreadsheet_id, wrapper_services)
-    await spreadsheets_update_value(
-        spreadsheet_id,
-        charity_projects,
-        wrapper_services
-    )
+    try:
+        await spreadsheets_update_value(
+            spreadsheet_id,
+            charity_projects,
+            wrapper_services
+        )
+    except ValueError as error:
+        raise HTTPException(
+            status_code=400,
+            detail=str(error)
+        )
     return spreadsheet_url
--- app/services/google_api.py
+++ app/services/google_api.py
@@ -29,7 +29,7 @@ SPREADSHEET_BODY = dict(
     )]
 )
 
-HEADER_BODY = [[TITLE.rsplit(maxsplit=1)],
+HEADER_BODY = [TITLE.rsplit(maxsplit=1),
                ['Топ проектов по скорости закрытия'],
                ['Название проекта', 'Время сбора', 'Описание']]
 
@@ -40,7 +40,11 @@ async def spreadsheets_create(
 ) -> tuple[str, str]:
     service = await wrapper_services.discover('sheets', 'v4')
     spreadsheet_body = copy.deepcopy(SPREADSHEET_BODY)
-    spreadsheet_body['properties']['title'].format(now_date_time)
+    spreadsheet_body['properties']['title'] = spreadsheet_body[
+        'properties'
+    ][
+        'title'
+    ].format(now_date_time)
     response = await wrapper_services.as_service_account(
         service.spreadsheets.create(json=spreadsheet_body)
     )
@@ -67,33 +71,36 @@ async def set_user_permissions(
 
 
 async def spreadsheets_update_value(
-    spreadsheetid: str,
+    spreadsheet_id: str,
     charity_projects: list,
     wrapper_services: Aiogoogle
 ) -> None:
     now_date_time = datetime.now().strftime(FORMAT)
     service = await wrapper_services.discover('sheets', 'v4')
     header_body = copy.deepcopy(HEADER_BODY)
-    header_body[0][1].format(now_date_time)
+    header_body[0][1] = header_body[0][1].format(now_date_time)
     table_values = [
-        *HEADER_BODY,
+        *header_body,
         *[[project.name,
            str(project.close_date - project.create_date),
            project.description]
           for project in charity_projects]
     ]
-    if (len(table_values) > ROW_COUNT or max(
-        table_values, key=len
-    ) > COLUMN_COUNT):
-        raise OverflowError('Размер данных слишком большой')
+    table_rows_count = len(table_values)
+    table_columns_count = max([len(row) for row in table_values])
+    if table_rows_count > ROW_COUNT or table_columns_count > COLUMN_COUNT:
+        raise ValueError(
+            f'Размер данных не должен превышать'
+            f' {ROW_COUNT} строк и {COLUMN_COUNT} столбцов'
+        )
     update_body = {
         'majorDimension': 'ROWS',
         'values': table_values
     }
     await wrapper_services.as_service_account(
         service.spreadsheets.values.update(
-            spreadsheetId=spreadsheetid,
-            range=f'R1C1:R{ROW_COUNT}C{COLUMN_COUNT}',
+            spreadsheetId=spreadsheet_id,
+            range=f'R1C1:R{table_rows_count}C{table_columns_count}',
             valueInputOption='USER_ENTERED',
             json=update_body
         )
